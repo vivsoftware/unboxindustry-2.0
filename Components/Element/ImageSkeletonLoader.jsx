5718a14b46e20affaa1a798f6371b85c4fec625b
ashootosh <ashootosh@unboxindustry.com>
2024-05-23 14:00:34 +0530

    dsa

---
+++ Components/Element/ImageSkeletonLoader.jsx
@@ -0,0 +1,10 @@
+// src/Components/Element/ImageSkeletonLoader.js
+import React from 'react';
+
+const ImageSkeletonLoaderBanner = () => {
+  return (
+    <div className="image-skeleton-loader-banner"></div>
+  );
+};
+
+export default ImageSkeletonLoaderBanner;
